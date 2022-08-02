4b78fbdc43d29a5e2a0aa69f7a91ba1a1a28988f
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-08-02 04:06:47 +0300

    react page test

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react'
 
 function App() {
+  useEffect(()=>{
+    fetch('/tutorials')
+    .then(res=> res.json())
+    .then(console.log)
+  })
   return (
     <div className="App">
       <header className="App-header">
