e726e7c5289c552f1ae62a67af555f77e81627ae
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-08-02 03:11:05 +0300

    add tutorial resource methods handlers

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -2,13 +2,35 @@ class TutorialsController < ApplicationController
 
     def index
         tutorials = Tutorial.all
-        render json: tutorials
+        render json: tutorials, status: :ok
 
     end
 
     def show
-        tutorials = Tutorial.find(params[:id])
-        render json: tutorials
+        tutorial = find_tutorial
+        render json: tutorial, status: :ok
+    end 
+
+    def create
+        tutorial = Tutorial.create(tutorial_params)
+        render json: tutorial, status: :created
+    end 
+
+    def update
+        tutorial = find_tutorial
+        tutorial.update(tutorial_params)
+        render json: tutorial, status: :created
+    end 
+
+
+
+    private 
+    def tutorial_params
+        params.permit(:image_url,:title,:description,:video_url)
+    end 
+
+    def find_tutorial
+        tutorial = Tutorial.find(params[:id])
     end 
 
 
