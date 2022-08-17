149e587f4918c58264c9e22fdcfd8419faffd98e
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-08-17 18:12:54 +0300

    fx merge conflicts & configure cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-    origins "*"
+    origins "https://zanbase-backend.herokuapp.com/"
 
      resource "*",
        headers: :any,
