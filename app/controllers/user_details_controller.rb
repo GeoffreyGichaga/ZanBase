63ad1ff051579d26f4dd44ecc12e411452c37d91
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-08-17 17:58:40 +0300

    fx merge conlfict

--- app/controllers/user_details_controller.rb
+++ app/controllers/user_details_controller.rb
@@ -1,13 +1,13 @@
 class UserDetailsController < ApplicationController
 
     def create
-        user = user.create!(user_payment_details_params)
+        user = UserDetail.create!(user_payment_details_params)
         render json: user, status: :created
     end
 
 
     def update
-        user = user.update!(user_payment_details_params)
+        user = UserDetail.update!(user_payment_details_params)
         render json: user, status: :created
     end 
 
@@ -17,6 +17,7 @@ class UserDetailsController < ApplicationController
 
 
     private
+
     def user_payment_details_params
         params.permit(:name_on_account,:bank_name,:account_number,:bank_code,:branch_code,:telephone_number)
     end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_one :user_detail
     
     validates :firstname, presence: true
     validates :lastname, presence: true
