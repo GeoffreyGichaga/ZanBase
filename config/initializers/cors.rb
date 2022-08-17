35e96c299fdd4ab5e51010f7a96a030949a70cca
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-08-17 19:16:07 +0300

    fx cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-    origins "https://zan-base.vercel.app/"
+    origins "https://zan-base.vercel.app"
 
      resource "*",
        headers: :any,
