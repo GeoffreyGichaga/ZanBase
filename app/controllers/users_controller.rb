5ffaee8f014dea4467fb65acd274da9a5d9728ff
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-08-20 09:10:00 +0300

    fx bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
     def create
         user = User.create!(user_params)
         session[:user_id] = user.id
-        render json: user ,status: :created
+
+        render json: user , status: :created
 
     end 
 
